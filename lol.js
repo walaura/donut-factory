656a384daa4220b43082c9c4b4fda6681d743925
Lau Skeeter <hi@laura.monster>
2020-04-09 12:08:28 +0100

    loool

---
+++ lol.js
@@ -0,0 +1,10 @@
+const lineReplace = require('line-replace');
+lineReplace({
+	file: './node_modules/postcss/lib/map-generator.js',
+	line: 193,
+	text: '',
+	addNewLine: true,
+	callback: () => {
+		console.log('unfucked parcel');
+	},
+});
