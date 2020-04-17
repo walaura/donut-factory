adb293bc5789eeaf0b19fe038e002a4136ee1777
Lau Skeeter <hi@laura.monster>
2020-04-17 18:09:16 +0100

    its the weekend screw this

--- src/canvas/sprite/world-tile.ts
+++ src/canvas/sprite/world-tile.ts
@@ -79,33 +79,53 @@ export const mkWorldTile = ({
 							y: row + atAbs.y / 20,
 						}) < 0.4,
 				};
-				let waterOnAfarSides = {
+
+				let waterOnCorners = {
 					top:
 						heightMapAtXY(10, {
-							x: col + atAbs.x / 20,
-							y: row - 2 + atAbs.y / 20,
-						}) < 0.4,
-					bottom:
-						heightMapAtXY(10, {
-							x: col + atAbs.x / 20,
-							y: row + 2 + atAbs.y / 20,
-						}) < 0.4,
-					left:
-						heightMapAtXY(10, {
-							x: col - 2 + atAbs.x / 20,
-							y: row + atAbs.y / 20,
+							x: col + 1 + atAbs.x / 20,
+							y: row + 1 + atAbs.y / 20,
 						}) < 0.4,
 					right:
 						heightMapAtXY(10, {
-							x: col + 2 + atAbs.x / 20,
-							y: row + atAbs.y / 20,
+							x: col + 1 + atAbs.x / 20,
+							y: row - 1 + atAbs.y / 20,
+						}) < 0.4,
+					bottom:
+						heightMapAtXY(10, {
+							x: col - 1 + atAbs.x / 20,
+							y: row - 1 + atAbs.y / 20,
+						}) < 0.4,
+					left:
+						heightMapAtXY(10, {
+							x: col - 1 + atAbs.x / 20,
+							y: row + 1 + atAbs.y / 20,
 						}) < 0.4,
 				};
 
-				let smoldetail = noiseMapAtXY({
-					x: col + atAbs.x / 20,
-					y: row + atAbs.y / 20,
-				});
+				let landOnSides = {
+					top:
+						heightMapAtXY(10, {
+							x: col + atAbs.x / 20,
+							y: row - 1 + atAbs.y / 20,
+						}) > 0.5,
+					bottom:
+						heightMapAtXY(10, {
+							x: col + atAbs.x / 20,
+							y: row + 1 + atAbs.y / 20,
+						}) > 0.5,
+					left:
+						heightMapAtXY(10, {
+							x: col - 1 + atAbs.x / 20,
+							y: row + atAbs.y / 20,
+						}) > 0.5,
+					right:
+						heightMapAtXY(10, {
+							x: col + 1 + atAbs.x / 20,
+							y: row + atAbs.y / 20,
+						}) > 0.5,
+				};
+				let color = 'water';
 
 				let isLand = largeriver > 0.5;
 				let props: TileProps = { type: 'full', color: 'water' };
@@ -113,52 +133,21 @@ export const mkWorldTile = ({
 				if (isLand) {
 					props.color = 'grass';
 					for (let direction of directions) {
-						if (waterOnSides[direction] && isLand) {
-							let otherDirections = directions.filter((d) => d !== direction);
-							props = {
-								type: 'half',
-								colors: ['water', 'dirt'],
-								direction,
-							};
-							for (let otherDirection of otherDirections) {
-								if (waterOnSides[otherDirection]) {
-									props = {
-										type: 'corner',
-										colors: ['water', 'dirt'],
-										direction: otherDirection,
-									};
-								}
-							}
+						if (waterOnSides[direction]) {
+							props.color = 'dirt';
 						}
-
-						if (
-							!waterOnSides[direction] &&
-							waterOnAfarSides[direction] &&
-							isLand
-						) {
-							let otherDirections = directions.filter((d) => d !== direction);
-							let replaceProps: ?TileProps = {
-								type: 'half',
-								colors: ['dirt', 'grass'],
-								direction,
-							};
-							for (let otherDirection of otherDirections) {
-								if (waterOnSides[otherDirection]) {
-									replaceProps = null;
-								} else if (waterOnAfarSides[otherDirection]) {
-									replaceProps = {
-										type: 'corner',
-										colors: ['dirt', 'grass'],
-										direction: otherDirection,
-									};
-								}
-							}
-							if (replaceProps) {
-								props = replaceProps;
-							}
+						if (waterOnCorners[direction]) {
+							props.color = 'dirt';
 						}
 					}
 					//ctx.globalAlpha = drawEdge ? 0.5 : 1;
+				}
+				if (!isLand) {
+					for (let direction of directions) {
+						if (landOnSides[direction]) {
+							props.color = 'HOTPINK4DEBUG';
+						}
+					}
 				}
 
 				/*
